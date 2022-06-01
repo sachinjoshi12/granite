3131d89a2d00a6300cbd59d1ae08917d3a9d3e8e
sachin joshi <sachin.joshi@bigbinary.com>
2022-06-01 23:35:21 +0530

    Set up react environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :tasks
 
-  root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
