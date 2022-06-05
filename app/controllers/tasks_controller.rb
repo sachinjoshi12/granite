0b65ea5155c18f663bc524c53fa652f738e63475
sachin joshi <sachin.joshi@bigbinary.com>
2022-06-05 23:01:04 +0530

    Added optimized slug generation method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  resources: tasks, param: :identifier_name
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
   end
-  resources: tasks, param: :identifier_name
   def show
-    @task = Task.find_by(identifier_name: params[:identifier_name]);
+    @task = Task.find_by(identifier_name: params[:identifier_name])
   end
 end
