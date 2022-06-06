2776f4293f9f53f6339511c4b587b616599ab7ed
sachin joshi <sachin.joshi@bigbinary.com>
2022-06-06 15:12:13 +0530

    Added show page for a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  resources: tasks, param: :identifier_name
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
   end
+
   def create
     task = Task.new(task_params)
     task.save!
-    render status: :ok, json: { notice: 'Task was successfully created' }
+    respond_with_success("Task was successfully created")
   end
 
   private
@@ -17,14 +17,4 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:title)
     end
-  def show
-    @task = Task.find_by(identifier_name: params[:identifier_name])
-  end
-  def create
-    task = Task.new(task_params)
-    task.save!
-    respond_with_success(t("successfully_created"))
-  end
-
-
 end
