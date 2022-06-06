fce190798ad659fb2f41b9d95ee61683dff98ba1
sachin joshi <sachin.joshi@bigbinary.com>
2022-06-06 15:56:13 +0530

    Added ability to update a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :load_task!, only: %i[show update]
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
@@ -12,9 +13,23 @@ class TasksController < ApplicationController
     respond_with_success("Task was successfully created")
   end
 
+  def show
+    respond_with_json({ task: @task })
+  end
+
+  def update
+    task = Task.find_by!(slug: params[:slug])
+    task.update!(task_params)
+    respond_with_success(t("successfully_updated"))
+  end
+
   private
+
+    def load_task!
+      @task = Task.find_by!(slug: params[:slug])
+    end
 
     def task_params
       params.require(:task).permit(:title)
-    end
+      end
 end
