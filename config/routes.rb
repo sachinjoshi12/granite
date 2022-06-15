902de59b96cf80d615ccbf20ad8d98d84784ba41
sachin joshi <35058362+sachinjoshi12@users.noreply.github.com>
2022-06-15 15:58:11 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
-require 'sidekiq/web'
-require 'sidekiq/cron/web'
+# frozen_string_literal: true
+
+require "sidekiq/web"
+require "sidekiq/cron/web"
 
 Rails.application.routes.draw do
+
   def draw(routes_name)
     instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
   end
 
   draw :sidekiq
 
-  #previous routes as is
+  constraints(lambda { |req| req.format == :json }) do
+    resources :tasks, except: %i[new edit], param: :slug
+    resources :users, only: %i[index create]
+    resource :session, only: [:create, :destroy]
+    resources :comments, only: :create
+    resource :preferences, only: %i[show update] do
+      patch :mail, on: :collection
+    end
+  end
+
+  root "home#index"
+  get "*path", to: "home#index", via: :all
+end
