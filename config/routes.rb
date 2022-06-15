5334a794eecf39dab7329fef40807db5b3eaf7f2
sachin joshi <sachin.joshi@bigbinary.com>
2022-06-16 00:31:31 +0530

    Code Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
-require 'sidekiq/web'
-require 'sidekiq/cron/web'
+# frozen_string_literal: true
+
+require "sidekiq/web"
+require "sidekiq/cron/web"
 
 Rails.application.routes.draw do
-  def draw(routes_name)
-    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(username),
+      ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(password),
+        ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
+
+  constraints(lambda { |req| req.format == :json }) do
+    resources :tasks, except: %i[new edit], param: :slug
+    resources :users, only: %i[create index]
+    resource :session, only: %i[create destroy]
+    resources :comments, only: :create
+    resource :preference, only: %i[show update] do
+      patch :mail, on: :collection
+    end
   end
 
-  draw :sidekiq
-
-  #previous routes as is
+  root "home#index"
+  get "*path", to: "home#index", via: :all
+end
